9eb6a69bfb846256535280b7cf29d965aa4c8790
jadilsonf <jadilsonfilho171292@gmail.com>
2022-10-26 12:28:17 -0300

    finalizando a sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar(){
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"/>
+            <div className={styles.profile}>
+
+                <img src="https://github.com/jadilsonf.png"/>
+
+
+                <strong>Jadilson Filho</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
